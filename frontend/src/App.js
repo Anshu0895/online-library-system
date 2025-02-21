65833cc353ea91d46cb1eed1aa0b44136eadd363
Anshu Anand <xs-514ansana@xs-host603-037.xs.neuralcompany.team>
2025-02-21 16:38:00 +0530

    issue fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes,useNavigate } from 'react-router-dom';
-import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/Signup';
-import Home from './pages/Home'
-import Navbar from './Components/Navbar'
-import { useState,useEffect } from 'react';
-import './App.css';
-import OwnerDashboard from './pages/OwnerDashboard';
-import ReaderDashboard from './pages/ReaderDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import api from './utils/api';
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Home from "./pages/Home";
+import Navbar from "./Components/Navbar";
+import { useState, useEffect } from "react";
+import "./App.css";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import ReaderDashboard from "./pages/ReaderDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import api from "./utils/api";
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
+    const storedToken = localStorage.getItem("token");
     if (storedToken) {
       setToken(storedToken);
       fetchUserData(storedToken);
@@ -28,7 +31,7 @@ const App = () => {
   }, []);
   const fetchUserData = async (token) => {
     try {
-      const response = await api.get('/users', {
+      const response = await api.get("/users", {
         headers: {
           Authorization: `${token}`,
         },
@@ -41,35 +44,35 @@ const App = () => {
 
   const handleLoginSuccess = (token) => {
     setToken(token);
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
     fetchUserData(token);
   };
   const handleLogout = () => {
     setToken(null);
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setUser(null);
-    navigate('/');
+    navigate("/");
   };
   return (
-    
-    
-      <>
+    <>
       <Navbar token={token} handleLogout={handleLogout} />
 
-     
-        <Routes>
-      
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/libraries" element={<OwnerDashboard token={token} />} />
-          <Route path="/reader" element={<ReaderDashboard token={token} user={user}/>} /> 
-          <Route path="/admin" element={<AdminDashboard token={token}/>} />
-        
-        </Routes>
-        <ToastContainer />
-        </>
-      
+        <Route
+          path="/login"
+          element={<Login onLoginSuccess={handleLoginSuccess} />}
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/libraries" element={<OwnerDashboard token={token} />} />
+        <Route
+          path="/reader"
+          element={<ReaderDashboard token={token} user={user} />}
+        />
+        <Route path="/admin" element={<AdminDashboard token={token} />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
